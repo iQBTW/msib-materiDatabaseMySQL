a129acfda59aa5c7e862937d525ae93b4c5e0e20
iQ <estrizqy@gmail.com>
2023-05-05 15:26:44 +0700

    update db

--- dbpos.sql
+++ dbpos.sql
@@ -1,308 +1,297 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.18  Distrib 10.4.17-MariaDB, for Win64 (AMD64)
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 12, 2023 at 09:58 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.2.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: dbpos
+-- ------------------------------------------------------
+-- Server version	10.4.17-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dbpos`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `jenis_produk`
 --
 
+DROP TABLE IF EXISTS `jenis_produk`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `jenis_produk` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(45) DEFAULT NULL,
-  `ket` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `jenis_produk`
 --
 
-INSERT INTO `jenis_produk` (`id`, `nama`, `ket`) VALUES
-(1, 'Elektronik', 'tersedia'),
-(2, 'Makanan', 'tersedia'),
-(3, 'Minuman', 'tidak tersedia'),
-(4, 'Furniture', 'tersedia');
-
--- --------------------------------------------------------
+LOCK TABLES `jenis_produk` WRITE;
+/*!40000 ALTER TABLE `jenis_produk` DISABLE KEYS */;
+INSERT INTO `jenis_produk` VALUES (1,'elektronik'),(2,'furniture'),(3,'makanan'),(4,'minuman'),(5,'komputer');
+/*!40000 ALTER TABLE `jenis_produk` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `kartu`
 --
 
+DROP TABLE IF EXISTS `kartu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `kartu` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `kode` varchar(6) DEFAULT NULL,
-  `nama` varchar(30) NOT NULL,
+  `nama` varchar(45) DEFAULT NULL,
   `diskon` double DEFAULT NULL,
-  `iuran` double DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `iuran` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `kode_UNIQUE` (`kode`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `kartu`
+--
+
+LOCK TABLES `kartu` WRITE;
+/*!40000 ALTER TABLE `kartu` DISABLE KEYS */;
+INSERT INTO `kartu` VALUES (1,'GOLD','Gold Utama',0.05,100000),(2,'PLAT','Platinum Jaya',0.1,150000),(3,'SLV','Silver',0.025,50000),(4,'NO','Non Member',0,0);
+/*!40000 ALTER TABLE `kartu` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `pelanggan`
 --
 
+DROP TABLE IF EXISTS `pelanggan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pelanggan` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `kode` varchar(10) DEFAULT NULL,
-  `nama_pelanggan` varchar(50) DEFAULT NULL,
+  `nama` varchar(45) DEFAULT NULL,
   `jk` char(1) DEFAULT NULL,
-  `alamat` varchar(40) DEFAULT NULL,
   `tmp_lahir` varchar(30) DEFAULT NULL,
   `tgl_lahir` date DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
-  `kartu_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `kartu_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_pelanggan_kartu1_idx` (`kartu_id`),
+  KEY `idx_nama_pelanggan` (`nama`),
+  KEY `idx_tgllahir_pelanggan` (`tgl_lahir`),
+  CONSTRAINT `fk_pelanggan_kartu1` FOREIGN KEY (`kartu_id`) REFERENCES `kartu` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `pelanggan`
 --
 
-INSERT INTO `pelanggan` (`id`, `kode`, `nama_pelanggan`, `jk`, `alamat`, `tmp_lahir`, `tgl_lahir`, `email`, `kartu_id`) VALUES
-(1, '5055', 'Rizqy', 'L', 'Jl.inijalan', 'Tabanan', '2000-12-21', 'estrizqy@gmail.com', 1),
-(2, '5056', 'Sandi', 'L', 'Jl.inijalan', 'Tabanan', '1999-02-08', 'iniemail1@gmail.com', 2),
-(3, '5057', 'Budi', 'L', 'Jl.inijalan', 'Malang', '2003-08-12', 'iniemail2@gmail.com', 3);
-
--- --------------------------------------------------------
+LOCK TABLES `pelanggan` WRITE;
+/*!40000 ALTER TABLE `pelanggan` DISABLE KEYS */;
+INSERT INTO `pelanggan` VALUES (1,'C001','Agung Sedayu','L','Solo','2010-01-01','sedayu@gmail.com',1),(2,'C002','Pandan Wangi','P','Yogyakarta','1950-01-01','wangi@gmail.com',2),(3,'C003','Sekar Mirah','P','Kediri','1983-02-20','mirah@yahoo.com',1),(4,'C004','Swandaru Geni','L','Kediri','1981-01-04','swandaru@yahoo.com',4),(5,'C005','Pradabashu','L','Pati','1985-04-02','prada85@gmail.com',2),(6,'C006','Gayatri Dwi','P','Jakarta','1987-11-28','gaya87@gmail.com',1),(7,'C007','Dewi Gyat','P','Jakarta','1988-12-01','giyat@gmail.com',1),(8,'C008','Andre Haru','L','Surabaya','1990-07-15','andre.haru@gmail.com',4),(9,'C009','Ahmad Hasan','L','Surabaya','1992-10-15','ahasan@gmail.com',4),(10,'C010','Cassanndra','P','Belfast','1990-11-20','casa90@gmail.com',1);
+/*!40000 ALTER TABLE `pelanggan` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `pembayaran`
 --
 
+DROP TABLE IF EXISTS `pembayaran`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pembayaran` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nokuitansi` varchar(10) DEFAULT NULL,
   `tanggal` date DEFAULT NULL,
   `jumlah` double DEFAULT NULL,
   `ke` int(11) DEFAULT NULL,
-  `pesanan_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `pesanan_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nokuitansi_UNIQUE` (`nokuitansi`),
+  KEY `fk_pembayaran_pesanan1_idx` (`pesanan_id`),
+  CONSTRAINT `fk_pembayaran_pesanan1` FOREIGN KEY (`pesanan_id`) REFERENCES `pesanan` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `pembayaran`
+--
+
+LOCK TABLES `pembayaran` WRITE;
+/*!40000 ALTER TABLE `pembayaran` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pembayaran` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `pembelian`
 --
 
+DROP TABLE IF EXISTS `pembelian`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pembelian` (
-  `id` int(11) NOT NULL,
-  `nopembelian` varchar(10) DEFAULT NULL,
-  `tanggal` date DEFAULT NULL,
-  `jumlah` double DEFAULT NULL,
-  `pembayaran_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tanggal` varchar(45) DEFAULT NULL,
+  `nomor` varchar(10) DEFAULT NULL,
+  `produk_id` int(11) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `harga` double DEFAULT NULL,
+  `vendor_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nomor_UNIQUE` (`nomor`),
+  KEY `produk_id` (`produk_id`),
+  KEY `vendor_id` (`vendor_id`),
+  CONSTRAINT `pembelian_ibfk_1` FOREIGN KEY (`produk_id`) REFERENCES `produk` (`id`),
+  CONSTRAINT `pembelian_ibfk_2` FOREIGN KEY (`vendor_id`) REFERENCES `vendor` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `pembelian`
+--
+
+LOCK TABLES `pembelian` WRITE;
+/*!40000 ALTER TABLE `pembelian` DISABLE KEYS */;
+INSERT INTO `pembelian` VALUES (1,'2019-10-10','P001',1,2,3500000,1),(2,'2019-11-20','P002',2,5,5500000,2),(3,'2019-12-12','P003',2,5,5400000,1),(4,'2020-01-20','P004',7,200,1800,3),(5,'2020-01-20','P005',5,100,2300,3);
+/*!40000 ALTER TABLE `pembelian` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `pesanan`
 --
 
+DROP TABLE IF EXISTS `pesanan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pesanan` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `tanggal` date DEFAULT NULL,
   `total` double DEFAULT NULL,
-  `pelanggan_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `pelanggan_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_pesanan_customer_idx` (`pelanggan_id`),
+  CONSTRAINT `fk_pesanan_customer` FOREIGN KEY (`pelanggan_id`) REFERENCES `pelanggan` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `pesanan`
+--
+
+LOCK TABLES `pesanan` WRITE;
+/*!40000 ALTER TABLE `pesanan` DISABLE KEYS */;
+INSERT INTO `pesanan` VALUES (1,'2015-11-04',9720000,1),(2,'2015-11-04',17500,3),(3,'2015-11-04',0,6),(4,'2015-11-04',0,7),(5,'2015-11-04',0,10),(6,'2015-11-04',0,2),(7,'2015-11-04',0,5),(8,'2015-11-04',0,4),(9,'2015-11-04',0,8),(10,'2015-11-04',0,9);
+/*!40000 ALTER TABLE `pesanan` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `pesanan_items`
 --
 
+DROP TABLE IF EXISTS `pesanan_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pesanan_items` (
-  `id` int(11) NOT NULL,
-  `nopesanan` varchar(15) DEFAULT NULL,
-  `tanggal` date DEFAULT NULL,
-  `pelanggan_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produk_id` int(11) NOT NULL,
+  `pesanan_id` int(11) NOT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `harga` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_pesanan_items_pesanan1_idx` (`pesanan_id`),
+  KEY `fk_pesanan_items_produk1_idx` (`produk_id`),
+  CONSTRAINT `fk_pesanan_items_pesanan1` FOREIGN KEY (`pesanan_id`) REFERENCES `pesanan` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_pesanan_items_produk1` FOREIGN KEY (`produk_id`) REFERENCES `produk` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `pesanan_items`
+--
+
+LOCK TABLES `pesanan_items` WRITE;
+/*!40000 ALTER TABLE `pesanan_items` DISABLE KEYS */;
+INSERT INTO `pesanan_items` VALUES (1,1,1,1,5040000),(2,3,1,1,4680000),(3,5,2,5,3500),(6,5,3,10,3500),(7,1,3,1,5040000),(9,5,5,10,3500),(10,5,6,20,3500);
+/*!40000 ALTER TABLE `pesanan_items` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `produk`
 --
 
+DROP TABLE IF EXISTS `produk`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `produk` (
-  `id` int(11) NOT NULL,
-  `kode_produk` varchar(10) DEFAULT NULL,
-  `nama_produk` varchar(45) DEFAULT NULL,
-  `harga_produk` double DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kode` varchar(10) DEFAULT NULL,
+  `nama` varchar(45) DEFAULT NULL,
   `harga_beli` double DEFAULT NULL,
+  `harga_jual` double DEFAULT NULL,
   `stok` int(11) DEFAULT NULL,
   `min_stok` int(11) DEFAULT NULL,
-  `jumlah_produk` int(11) DEFAULT NULL,
-  `jenis_produk_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `jenis_produk_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `kode_UNIQUE` (`kode`),
+  KEY `fk_produk_jenis_produk1_idx` (`jenis_produk_id`),
+  CONSTRAINT `fk_produk_jenis_produk1` FOREIGN KEY (`jenis_produk_id`) REFERENCES `jenis_produk` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `produk`
 --
 
-INSERT INTO `produk` (`id`, `kode_produk`, `nama_produk`, `harga_produk`, `harga_beli`, `stok`, `min_stok`, `jumlah_produk`, `jenis_produk_id`) VALUES
-(1, '2011', 'TV', 2000000, 1000000, 20, 1, 100, 1),
-(7, '2012', 'Mouse', 200000, 90000, 15, 3, 30, 1),
-(8, '2013', 'Fried Chicken', 8000, 5000, 50, 1, 150, 2),
-(9, '2014', 'Lemari', 1200000, 500000, 30, 1, 50, 4),
-(10, '2015', 'Es Teh', 3000, 2000, 50, 1, 120, 3);
-
--- --------------------------------------------------------
+LOCK TABLES `produk` WRITE;
+/*!40000 ALTER TABLE `produk` DISABLE KEYS */;
+INSERT INTO `produk` VALUES (1,'TV01','Televisi 21 inch',3500000,5040000,5,2,1),(2,'TV02','Televisi 40 inch',5500000,7440000,4,2,1),(3,'K001','Kulkas 2 pintu',3500000,4680000,6,2,1),(4,'M001','Meja Makan',500000,600000,4,3,2),(5,'TK01','Teh Kotak',3000,3500,6,10,4),(6,'PC01','PC Desktop HP',7000000,9600000,9,2,5),(7,'TB01','Teh Botol',2000,2500,53,10,4),(8,'AC01','Notebook Acer',8000000,10800000,7,2,5),(9,'LN01','Notebook Lenovo',9000000,12000000,9,2,5),(10,'L004','Laptop HP',12000000,13000000,20,5,5);
+/*!40000 ALTER TABLE `produk` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `vendor`
 --
 
+DROP TABLE IF EXISTS `vendor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `vendor` (
-  `id` int(11) NOT NULL,
-  `nama_vendor` varchar(40) DEFAULT NULL,
-  `alamat_vendor` varchar(40) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nomor` varchar(4) NOT NULL,
+  `nama` varchar(40) NOT NULL,
+  `kota` varchar(30) DEFAULT NULL,
+  `kontak` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nomor` (`nomor`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `vendor`
 --
 
---
--- Indexes for table `jenis_produk`
---
-ALTER TABLE `jenis_produk`
-  ADD PRIMARY KEY (`id`);
+LOCK TABLES `vendor` WRITE;
+/*!40000 ALTER TABLE `vendor` DISABLE KEYS */;
+INSERT INTO `vendor` VALUES (1,'V001','PT Guna Samudra','Surabaya','Ali Nurdin'),(2,'V002','PT Pondok C9','Depok','Putri Ramadhani'),(3,'V003','CV Jaya Raya Semesta','Jakarta','Dwi Rahayu'),(4,'V004','PT Lekulo X','Kebumen','Mbambang G'),(5,'V005','PT IT Prima','Jakarta','David W');
+/*!40000 ALTER TABLE `vendor` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for table `kartu`
---
-ALTER TABLE `kartu`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `kode` (`kode`);
-
---
--- Indexes for table `pelanggan`
---
-ALTER TABLE `pelanggan`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `kode` (`kode`);
-
---
--- Indexes for table `pembayaran`
---
-ALTER TABLE `pembayaran`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nokuitansi` (`nokuitansi`);
-
---
--- Indexes for table `pembelian`
---
-ALTER TABLE `pembelian`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nopembelian` (`nopembelian`);
-
---
--- Indexes for table `pesanan`
---
-ALTER TABLE `pesanan`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `pesanan_items`
---
-ALTER TABLE `pesanan_items`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nopesanan` (`nopesanan`);
-
---
--- Indexes for table `produk`
---
-ALTER TABLE `produk`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `kode_produk` (`kode_produk`);
-
---
--- Indexes for table `vendor`
---
-ALTER TABLE `vendor`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `jenis_produk`
---
-ALTER TABLE `jenis_produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `kartu`
---
-ALTER TABLE `kartu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pelanggan`
---
-ALTER TABLE `pelanggan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `pembayaran`
---
-ALTER TABLE `pembayaran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pembelian`
---
-ALTER TABLE `pembelian`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pesanan`
---
-ALTER TABLE `pesanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pesanan_items`
---
-ALTER TABLE `pesanan_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `produk`
---
-ALTER TABLE `produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `vendor`
---
-ALTER TABLE `vendor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-19  0:42:31
